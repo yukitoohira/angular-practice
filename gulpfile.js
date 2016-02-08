e8f2fe69954871f96a77803f20867f8b4c866c07
yohira0616 <yo.hira.dev@gmail.com>
2016-02-08 23:04:00 +0900

    gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,25 @@ var uglify = require('gulp-uglify');
 var babel = require('gulp-babel');
 var plumber=require('gulp-plumber');
 var concat=require('gulp-concat');
+var browser=require('browser-sync');
 
 gulp.task('build', function () {
-    gulp.src('src/index.js').pipe(plumber()).pipe(webpack({
-        output: {
-            filename: 'bundle.js'
-        }
-    })).pipe(babel()).pipe(uglify()).pipe(gulp.dest('dist'));
+    gulp.src('src/**/*.js')
+        .pipe(plumber())
+        .pipe(concat('bundle.js'))
+        .pipe(babel())
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function () {
     gulp.watch('src/**/*.js', ['build']);
 });
 
-
+gulp.task('serve',function(){
+    browser({
+        server:{
+            baseDir:"./"
+        }
+    });
+});
